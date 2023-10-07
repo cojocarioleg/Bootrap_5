5b4dd1482984adc6962ecbdcc85fc20f267900e2
Oleg <cojocari_oleg@yahoo.com>
2023-10-07 10:37:42 +0300

    owl-carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 window.addEventListener('scroll', function () {
     document.getElementById('header-nav').classList.toggle('header-nav-scroll', window.scrollY > 135);
 });
+
+$(document).ready(function () {
+    $(".owl-carousel-full").owlCarousel({
+        margin: 20,     
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            700: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+});
